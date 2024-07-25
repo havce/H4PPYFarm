c1757d81d68250e916ae9a985cac3a28d13ac344
Mark <25851052+markx86@users.noreply.github.com>
2024-07-25 12:42:28 +0200

    Discard teams on which the exploit doesn't work

--- client/start_sploit.py
+++ client/start_sploit.py
@@ -3,17 +3,23 @@ import math
 import os
 import re
 import sys
+from random import randint
 from time import time, sleep
-from requests import Session
-from requests import ConnectionError
+from requests import Session, ConnectionError
 from json import JSONDecodeError
 from subprocess import run as run_process, CalledProcessError, TimeoutExpired
 from concurrent.futures import ThreadPoolExecutor
 
 
+# global configuration
 cfg = {}
+
+# wave state
 flags = []
 wave = 1
+
+# failure filter
+failure_counters = {}
 
 BLACK = 0
 RED = 1
@@ -37,18 +43,25 @@ def usage():
     print(f"""USAGE: {sys.argv[0]} [OPTIONS] EXPLOIT
 
 The possible value for OPTIONS are:
-  --server-url URL         The URL of the server running H4PPY Farm
-  --server-pass PASSWORD   The password of the H4PPY Farm server
-  --timeout TIMEOUT        The amount of time in seconds after which an instance of the exploit should be killed
-  --help                   Print this message
+  --server-url URL         The URL of the server running H4PPY Farm.
+  --server-pass PASSWORD   The password of the H4PPY Farm server.
+  --timeout TIMEOUT        The amount of time in seconds after which an instance of the exploit should be killed.
+  --always-retry           Always try exploit on targets on which it always seems to fail.
+  --failure-threshold N    The number of consecutive failures for one team, after which the script should start
+                           decreasing the probability of running the exploit on that one team.
+  --max-failures MAX       The maximum amount of failures after which the probability of running the exploit stops
+                           decreasing.
+  --help                   Print this message.
     """)
     exit(-1)
 
 
-def get_arg(arg_name: str, default: str | int | None) -> str | None:
+def get_arg(arg_name: str, default: str | int | None, is_switch: bool) -> str | bool | None:
     try:
         idx = sys.argv.index(f"--{arg_name}")
-        if idx >= len(sys.argv) - 1:
+        if is_switch:
+            return not default
+        elif idx >= len(sys.argv) - 1:
             usage()
         return sys.argv[idx + 1]
     except ValueError:
@@ -58,13 +71,20 @@ def get_arg(arg_name: str, default: str | int | None) -> str | None:
 def parse_args():
     if "--help" in sys.argv:
         usage()
-    config_keys = {"server-url": None, "server-pass": None, "timeout": 10}
+    config_keys = {
+        "server-url": None,
+        "server-pass": None,
+        "timeout": 10,
+        "always-retry": False,
+        "max-failures": 12,
+        "failure-threshold": 4
+    }
     for arg, default in config_keys.items():
-        if not (arg_val := get_arg(arg, default)):
-            if not default:
+        if not (arg_val := get_arg(arg, default, isinstance(default, bool))):
+            if default is None:
                 usage()
             arg_val = default
-        elif isinstance(default, int):
+        elif isinstance(default, int | float):
             arg_val = float(arg_val)
         cfg[arg] = arg_val
     cfg["exploit"] = os.path.join("./", sys.argv[-1])
@@ -114,6 +134,9 @@ def get_config(session: Session):
     if not ("teams" in cfg):
         print("No configuration loaded!")
         exit(-1)
+    else:
+        for team in filter(lambda x: not (x in failure_counters), cfg["teams"]):
+            failure_counters[team] = 0
 
 
 def check_exploit():
@@ -121,12 +144,6 @@ def check_exploit():
     print(f"Checking exploit '{exploit}'...")
     try:
         with open(exploit, "r") as f:
-            first_line = f.readline()
-            # This check can be easily broken, I don't care
-            if not first_line.startswith("#!/usr/bin/env python3"):
-                print("Currently only python3 scripts are supported.\n"
-                      "Please put '#!/usr/bin/env python3' in the first line of the file")
-                exit(-1)
             source = "\n".join(f.readlines())
             if re.search(r'flush\s*=\s*True', source) is None:
                 print("Please use print(..., flush=True) in your script, instead of just print(...)")
@@ -137,15 +154,29 @@ def check_exploit():
 
 
 def run_exploit(team: str) -> list[dict[str, str | float]] | None:
+    failure_threshold = cfg["failure-threshold"]
+    if randint(0, failure_counters[team]) > failure_threshold:
+        # decrease the possibility of running the exploit on teams on which the exploit seems to fail the most
+        wprint(highlight(f"Not running exploit on {team} (too many failures)", YELLOW))
+        return None
+    flag_format = cfg["flag_format"]
+    exploit = cfg["exploit"]
+    timeout = cfg["timeout"] if cfg["timeout"] > 1 else 1
+    args = ["python3", exploit, team]
+
     try:
-        flag_format = cfg["flag_format"]
-        exploit = cfg["exploit"]
-        timeout = cfg["timeout"] if cfg["timeout"] > 1 else 1
-        output = run_process([exploit, team], capture_output=True, timeout=timeout, check=True).stdout.decode()
+        # FIXME: Do NOT run all exploits with python3 by default. Check whether the file is a binary
+        #        or a script and either use the correct interpreter or refuse to run the file and
+        #        exit with an error.
+        output = run_process(args, capture_output=True, timeout=timeout, check=True).stdout.decode()
         run_flags = flag_format.findall(output)
         if len(run_flags) == 0:
             wprint(highlight(f"Got no flags for team {team}", MAGENTA))
         else:
+            if failure_counters[team] > failure_threshold:
+                failure_counters[team] = failure_threshold  # give it another chance
+            else:
+                failure_counters[team] -= 1
             wprint(highlight(f"Got {len(run_flags)} flags from team {team}", GREEN))
             ts = time()
             return list(map(lambda x: {"flag": x, "ts": ts}, run_flags))
@@ -153,12 +184,15 @@ def run_exploit(team: str) -> list[dict[str, str | float]] | None:
         wprint(highlight(f"Exploit crashed on team {team}!", RED))
     except TimeoutExpired:
         wprint(highlight(f"Exploit timed-out on team {team}!", YELLOW))
+    if failure_counters[team] < cfg["max-failures"]:
+        failure_counters[team] += 1
     return None
 
 
 def compute_n_workers(n_workers: int, deadline: float, wave_time: float) -> int:
     teams = cfg["teams"]
 
+    wave_time = math.ceil(wave_time)
     teams_per_worker = math.ceil(len(teams) / n_workers)
     time_per_team = wave_time / teams_per_worker
     n_workers = math.ceil((time_per_team * len(teams)) / deadline)
@@ -189,8 +223,8 @@ def run_exploit_on_teams(n_workers: int) -> (float, list[dict[str, str | float]]
 def send_flags(session: Session):
     try:
         exploit = cfg["exploit"]
-        exploit_name, = os.path.basename(exploit).split(".", 1)
-        res = session.post(url_for(f"/api/flags/{exploit_name}"), json=flags)
+        exploit_name, _ = os.path.basename(exploit).split(".", 1)
+        res = session.post(url_for(f"/api/flags/{exploit_name}"), json=flags, timeout=10)
         if res.status_code != 200:
             wprint(highlight("Could not send flags, am I not authenticated?", YELLOW))
         else:
